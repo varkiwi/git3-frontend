ab4db76d708ab579109a5f3db793293d8a0bbbd4
jacekv <jaca347@gmail.com>
2021-06-25 20:22:08 +0200

    Adding route for repositories

---
+++ src/components/RepoContent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>User Address: {{ $route.params.userAddress }}</p>
+    <p>Repository Name: {{ $route.params.repositoryName }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'RepoContent',
+
+    data: () => ({}),
+};
+</script>
---
+++ src/views/Repository.vue
@@ -0,0 +1,15 @@
+<template>
+  <repo-content />
+</template>
+
+<script>
+import RepoContent from '../components/RepoContent.vue';
+
+export default {
+    name: 'Repository',
+
+    components: {
+        RepoContent,
+    },
+};
+</script>
