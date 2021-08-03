c3c1f7a4d241664bf1fcf845b586ba048e659c09
jacekv <jaca347@gmail.com>
2021-08-03 21:43:53 +0200

    Adjusting elements

--- src/views/Repository.vue
+++ src/views/Repository.vue
@@ -1,49 +1,61 @@
 <template>
-  <!-- <repo-content /> -->
-  <div class="px-8 pt-3">
-    <!-- this is the header part -->
-    <div class="d-flex mb-3">
-      <h1>Repository Name: {{ $route.params.repositoryName }}</h1>
-      <!-- <p>User Address: {{ $route.params.userAddress }}</p> -->
-    </div>
-    <!-- This is the tabs parts -->
-    <div>
-      <v-tabs background-color="secondary">
+  <div>
+    <div class="mb-5">
+      <div class="px-8 pt-3">
+        <!-- this is the header part -->
+        <div class="d-flex mb-3">
+          <h1>Repository Name: {{ $route.params.repositoryName }}</h1>
+          <!-- <p>User Address: {{ $route.params.userAddress }}</p> -->
+        </div>
+        <!-- This is the tabs parts -->
+        <div>
+          <v-tabs background-color="secondary">
 
-        <v-tabs-slider color="white"></v-tabs-slider>
+            <v-tabs-slider color="white"></v-tabs-slider>
 
-        <v-tab v-for="tab in tabs" :key="tab.name"
-          :id="tab.name"
-          @click="handleTabs"
-          class="white--text"
-        >
-          {{ tab.name }}
-        </v-tab>
-      </v-tabs>
+            <v-tab v-for="tab in tabs" :key="tab.name"
+              :id="tab.name"
+              @click="handleTabs"
+              class="white--text"
+            >
+              {{ tab.name }}
+            </v-tab>
+          </v-tabs>
+        </div>
+      </div>
+
+      <v-divider/>
     </div>
+
+    <RepositoryCode v-if="selectedTab == '/code'" />
+    <!-- <IssuesList v-else-if="selectedTab == '/issues'" /> -->
+
   </div>
 </template>
 
 <script>
-// import RepoContent from '../components/RepoContent.vue';
+import RepositoryCode from './RepositoryCode.vue';
+// import IssuesList from '../components/IssuesList.vue';
 
 export default {
     name: 'Repository',
 
+    components: {
+        RepositoryCode,
+        // IssuesList,
+    },
+
     data: () => ({
+        selectedTab: '/code',
         tabs: [
             { name: '/code' },
             { name: '/issues' },
         ],
     }),
     methods: {
-        handleTabs: (e) => {
-            console.log('Yes man', e.target.id);
+        handleTabs(e) {
+            this.selectedTab = e.target.id;
         },
     },
-
-    // components: {
-    //     RepoContent,
-    // },
 };
 </script>
---
+++ src/views/RepositoryCode.vue
@@ -0,0 +1,76 @@
+<template>
+  <v-container class="px-md-4 px-lg-5">
+    <v-row align="center" justify="center">
+
+      <!-- <v-col cols="2"> -->
+      <v-col>
+        <div class="d-flex flex-row">
+          <!-- Dropdown menu where we can switch the branch -->
+          <v-menu offset-y>
+
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn dark v-bind="attrs" v-on="on">
+                {{ items[model].title }}
+              </v-btn>
+            </template>
+
+            <v-list>
+              <v-list-item-group v-model="model">
+                <v-list-item
+                  v-for="(item, index) in items"
+                  :key="index"
+                  link
+                >
+                  <v-list-item-title>{{ item.title }}</v-list-item-title>
+                </v-list-item>
+              </v-list-item-group>
+            </v-list>
+
+          </v-menu>
+
+          <p class="pt-3">
+            <v-icon dark small>
+              mdi-source-branch
+            </v-icon>
+            x branches
+          </p>
+
+        </div>
+      </v-col>
+
+      <!-- <v-col> -->
+        <!-- Displayes the number of active branches -->
+        <!-- <p class="pt-3">
+          <v-icon dark small>
+            mdi-source-branch
+          </v-icon>
+          x branches
+        </p>
+      </v-col> -->
+
+      <v-spacer></v-spacer>
+
+      <v-col cols="1">
+        <!-- Download button -->
+        <v-btn>Download</v-btn>
+      </v-col>
+
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+    name: 'RepositoryCode',
+
+    data: () => ({
+        items: [
+            { title: 'main' },
+            { title: 'branch 1' },
+            { title: 'branch 2' },
+        ],
+        model: 0,
+    }),
+};
+</script>
