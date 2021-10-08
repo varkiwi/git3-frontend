7f1917b26b03e401e1cdfb49394918a4f81a3ef5
jacekv <jaca347@gmail.com>
2021-10-08 14:32:55 +0200

    Adding web3Provider to store

--- src/store/index.js
+++ src/store/index.js
@@ -8,9 +8,13 @@ export default new Vuex.Store({
         walletActive: false,
         walletAddress: null,
         openedDialogOnce: false,
+        web3Provider: null,
     },
     mutations: {
         setActive(state, active) {
+            if (!active) {
+                state.web3Provider = null;
+            }
             state.walletActive = active;
         },
         setWalletAddress(state, walletAddress) {
@@ -18,6 +22,9 @@ export default new Vuex.Store({
         },
         setOpenedDialogOnce(state, status) {
             state.openedDialogOnce = status;
+        },
+        setWeb3Provider(state, provider) {
+            state.web3Provider = provider;
         },
     },
     actions: {
